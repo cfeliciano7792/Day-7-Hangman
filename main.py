906c158ec7dd756c30a875b799a23b2b0804df49
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-26 18:40:15 +0000

    Added display list

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ word_list = ["aardvark", "baboon", "camel"]
 #picks a random word from the word_list and assign it to a variable called chosen_word
 chosen_word = random.choice(word_list)
 
+#testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+
+#created an empty list called display
+display = []
+
+#For each letter in the chosen_word, add a "_" to 'display'
+for letter in chosen_word:
+  display.append("_")
+
+print(display)
+
+
+
+
+
+
 #ask the user to guess a letter and makes the guess lowercase.
 guess = input("Guess a letter: ").lower()
 
